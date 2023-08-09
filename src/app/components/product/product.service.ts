dba72ab14aa886d15dec03891acead67dea59d40
Pavel Vavilin <iq23life@gmail.com>
2023-08-09 09:37:32 +0300

    create modal app product

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse, HttpParams } from "@angular/common/http";
-import { Observable, catchError, retry, throwError } from "rxjs";
+import { Observable, catchError, map, of, retry, throwError } from "rxjs";
 import { ErrorService } from "src/app/services/error.service";
 import { Product } from "src/app/models/product";
+import { CountryDTO } from "src/app/models/country";
+import { Countries } from "src/app/data/countries";
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +26,23 @@ export class ProductsService {
       retry(2),
       catchError(this.errorHandler.bind(this))
     )
+  }
+
+  create(product:any): Observable<any> {
+    return this.http.post<Product[]>('https://fakestoreapi.com/products', product)
+  }
+
+  getCountry(): Observable<CountryDTO[]> {
+    return this.http
+      .get<CountryDTO[]>(`https://fakestoreapi.com/products`)
+      .pipe(
+        catchError(() => {
+          return of([] as CountryDTO[]);
+        }),
+        map(() =>
+        Countries
+        )
+      );
   }
 
   private errorHandler(error: HttpErrorResponse) {
